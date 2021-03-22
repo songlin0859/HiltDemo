208ca671901a59f0eba21d854855108bbaecdeda
songlin <songlin09@qq.com>
2021-03-22 22:22:41 +0800

    provides module singleton (和dagger2基本一样)

--- app/src/main/java/com/sl/hiltdemo/MainActivity.kt
+++ app/src/main/java/com/sl/hiltdemo/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import com.sl.hiltdemo.data.User
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -12,11 +14,20 @@ class MainActivity : AppCompatActivity() {
 
     @Inject
     lateinit var user: User
+    @Inject
+    lateinit var retrofit: Retrofit
+    @Inject
+    lateinit var retrofit1: Retrofit
+    @Inject
+    lateinit var okHttpClient: OkHttpClient
+    @Inject
+    lateinit var okHttpClient1: OkHttpClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        textView.text = user.name
+        //textView.text = user.name
+        textView.text = "retrofit === retrofit1 :${retrofit === retrofit1} and okHttpClient === okHttpClient1 : ${okHttpClient === okHttpClient1}"
     }
 }
